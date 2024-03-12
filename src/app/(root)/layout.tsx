74a7f378a197c234d499812b8e5418c0c2bb6c11
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-03-12 11:08:22 +0330

    theme action button

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import ThemeButton from "@/components/shared/ThemeButton";
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 relative">
+        <ThemeButton />
+      <div className="flex">
+        RightSidebar
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
