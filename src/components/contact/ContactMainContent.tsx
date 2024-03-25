f11caaae22b6d130e2e6a67cdf2a3ee4ba8c5a55
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-03-25 12:38:53 +0330

    contact ui done

--- src/components/contact/ContactMainContent.tsx
+++ src/components/contact/ContactMainContent.tsx
@@ -1,8 +1,19 @@
+import ContactForm from "../forms/ContactForm";
+import TitleHeading from "../shared/TitleHeading";
 
 const ContactMainContent = () => {
   return (
-    <div>ContactMainContent</div>
-  )
-}
+    <div className="flex min-h-screen w-full flex-col items-center justify-center">
+      <div className="w-full md:w-[95%] xl:w-[80%]">
+            <TitleHeading
+        heading="با من تماس بگیرید"
+        text="به من پیام بدهید تا همین امروز پروژه‌ی شما را آغاز کنیم."
+      />
+      <ContactForm />
+      </div>
+  
+    </div>
+  );
+};
 
-export default ContactMainContent
+export default ContactMainContent;
---
+++ src/components/shared/TitleHeading.tsx
@@ -0,0 +1,16 @@
+import Divider from "@/common/Divider";
+interface Prop {
+  heading: string;
+  text: string;
+}
+const TitleHeading = ({ heading, text }: Prop) => {
+  return (
+    <div className="text-dark200_light800 mb-10 flex flex-col gap-2 text-center">
+      <h2 className="text-2xl font-bold">{heading}</h2>
+      <Divider otherClass="border-blue-600 flex self-center dark:border-blue-600 border-t-4 w-1/5" />
+      <p className="text-lg">{text}</p>
+    </div>
+  );
+};
+
+export default TitleHeading;
