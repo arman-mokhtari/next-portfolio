65432a28653dd5c45013baedfcea852f149f9f7e
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-03-26 08:07:19 +0330

    checkRole function added

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,18 +1,16 @@
-import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { checkRole } from "@/utils/roles";
 
 export default function AdminDashboard() {
-  const { sessionClaims } = auth();
-
   // If the user does not have the admin role, redirect them to the home page
-  if (sessionClaims?.metadata.role !== "admin") {
+  if (!checkRole("admin")) {
     redirect("/404");
   }
 
   return (
     <>
       <h1>This is the admin dashboard</h1>
-      <p>This page is restricted to users with the &apos;admin&apos; role.</p>
+      <p>This page is restricted to users with the admin role.</p>
     </>
   );
 }
