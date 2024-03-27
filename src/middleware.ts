d9a662f2096aaa7642ebbedb8af0f10cc191f234
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-03-27 09:30:53 +0330

    webhook route added to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,14 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
+    "/api/webhook",
     "/about",
     "/resume",
     "/skills",
     "/activities",
     "/contact",
   ],
+  ignoredRoutes: ["/api/webhook"],
 });
 
 export const config = {
