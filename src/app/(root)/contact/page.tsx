b5bccedfb02e2ad66df0d0b3ed44070b8a1331db
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-04-19 18:17:29 +0330

    contact page skeleton implemented

--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -1,6 +1,9 @@
 import ContactMainContent from "@/components/contact/ContactMainContent";
+import ContactSkeleton from "@/components/contact/ContactSkeleton";
 
 const Page = () => {
+  const loading = true;
+  if (loading) return <ContactSkeleton />;
   return <ContactMainContent />;
 };
 
