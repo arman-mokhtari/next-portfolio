e715316fde085a49e8d573e0502f477caf5581ba
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-04-19 18:19:15 +0330

    little changes

--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -1,9 +1,6 @@
 import ContactMainContent from "@/components/contact/ContactMainContent";
-import ContactSkeleton from "@/components/contact/ContactSkeleton";
 
 const Page = () => {
-  const loading = true;
-  if (loading) return <ContactSkeleton />;
   return <ContactMainContent />;
 };
 
