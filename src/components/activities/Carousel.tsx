544d4ff00012e1e3f37415a87960bde5e2ea08b0
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-05-18 08:35:06 +0330

    carousel changed to shadcn

--- src/components/activities/Carousel.tsx
+++ src/components/activities/Carousel.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import { settings } from "@/constants/sliderSettings";
-import Image from "next/image";
-import Slider from "react-slick";
 import TitleHeading from "../shared/TitleHeading";
+import { Card, CardContent } from "@/components/ui/card";
+import Autoplay from "embla-carousel-autoplay";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import Link from "next/link";
 
 interface AdminType {
   activities: {
@@ -19,24 +28,53 @@ interface ProjectSliderProps {
 
 const ProjectSlider = ({ admin }: ProjectSliderProps) => {
   return (
-    <div className="absolute w-full md:w-[95%] xl:w-[95%]">
+    <div className="w-full md:w-[95%]">
       <TitleHeading
         heading={admin.activities.title}
         text={admin.activities.desc}
       />
-      <Slider {...settings}>
-        {admin.activities.activityLinks.map((item, i) => (
-          <div className="flex-imp flex justify-center" key={i}>
-            <Image
-              className="object-cover sm:p-4"
-              alt="تصویر پروژه"
-              src={item}
-              width={450}
-              height={450}
-            />
-          </div>
-        ))}
-      </Slider>
+      <div className="flex justify-center px-6">
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+          className="w-full"
+        >
+          <CarouselContent>
+            {admin.activities.activityLinks.map((item, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex items-center justify-center p-0">
+                      <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={item.split("&")[1]}
+                      >
+                        <Image
+                          className="size-full object-cover"
+                          alt="Project image"
+                          src={item.split("&")[0]}
+                          width={450}
+                          height={450}
+                        />
+                      </Link>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   );
 };
